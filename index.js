de77821968cfd2e8381f059ea7f04f55838dab94
wasieDev <127023454+rens20@users.noreply.github.com>
2023-10-24 17:51:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
 const inputBox = document.getElementById("input-box");
 const button = document.querySelector("button");
 const list = document.getElementById("list-container");
- 
+let tasks = [];
+
 function addTask() {
   if (inputBox.value === "") {
-    alert("you must write something!");
+    alert("You must write something!");
   } else {
     let category = document.getElementById("category").value;
-    let li = document.createElement("li");
     let dueDate = document.getElementById("due-date").value;
-     let currentDate = new Date();
+    let currentDate = new Date();
     let taskDetails = `${category}: ${inputBox.value} (Due: ${dueDate})`;
-    li.innerHTML = taskDetails;
-    list.appendChild(li);
-    inputBox.value = "";
-    let span = document.createElement("span");
-    span.innerHTML = "x";
-    li.appendChild(span);
 
+    let task = {
+      details: taskDetails,
+      dueDate: dueDate,
+    };
+
+    tasks.push(task);
+    updateList();
+
+    inputBox.value = "";
 
     // Add notification here
     let notificationTime = new Date(dueDate);
     if (notificationTime > currentDate) {
       const timeDiff = notificationTime - currentDate;
       setTimeout(() => {
-       alert(`Your task ${inputBox.value} is due now!`);
-    }, timeDiff);
+        alert(`Your task ${inputBox.value} is due now!`);
+      }, timeDiff);
     }
   }
-  saveDate();
+  saveData();
 }
+
+// Function to update the list
+function updateList() {
+  list.innerHTML = "";
+  tasks.forEach((task) => {
+    let li = document.createElement("li");
+    li.innerHTML = task.details;
+    list.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "x";
+    li.appendChild(span);
+  });
+}
+
 // Function to handle task clicks and deletion
 list.addEventListener("click", (e) => {
   if (e.target.tagName === "LI") {
-    // Toggle the checked class for the task
     e.target.classList.toggle("checked");
-    // Save the updated data
     saveData();
   } else if (e.target.tagName === "SPAN") {
-    // Remove the task if the delete button is clicked
-    e.target.parentElement.remove();
-    // Save the updated data
+    const index = Array.from(e.target.parentNode.parentNode.children).indexOf(
+      e.target.parentNode
+    );
+    tasks.splice(index, 1);
+    updateList();
     saveData();
   }
 });
@@ -50,23 +67,24 @@ button.addEventListener("click", addTask);
 
 // Function to save the data to local storage
 function saveData() {
-  localStorage.setItem("data", list.innerHTML);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-// Function to show tasks on page load
-function showTask() {
-  const savedData = localStorage.getItem("data");
-  if (savedData) {
-    list.innerHTML = savedData;
+// Function to load the data from local storage
+function loadData() {
+  const data = localStorage.getItem("tasks");
+  if (data) {
+    tasks = JSON.parse(data);
+    updateList();
   }
 }
 
 // Event listener for the page load event
-window.addEventListener("load", showTask);
+window.addEventListener("load", loadData);
 
 // Event listener for the beforeunload event
 window.addEventListener("beforeunload", function (e) {
-  notifications.forEach(notification => clearTimeout(notification));
+  saveData();
   e.preventDefault();
-  e.returnValue = '';
+  e.returnValue = "";
 });
